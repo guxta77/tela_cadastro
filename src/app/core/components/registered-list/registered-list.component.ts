e8d6de6f07dd35b5f11ae0871c867cbbed6b0b6d
guxta77 <gustavoveroni10@gmail.com>
2024-09-01 02:12:19 -0300

    ajustes no resgistered-list

--- src/app/core/components/registered-list/registered-list.component.ts
+++ src/app/core/components/registered-list/registered-list.component.ts
@@ -1,24 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import { ItemService } from './services/item.service';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { BackButtonComponent } from '../back-button/back-button.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registered-list',
   standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, BackButtonComponent],
   templateUrl: './registered-list.component.html',
   styleUrls: ['./registered-list.component.scss']
 })
-export class RegisteredListComponent implements OnInit {
+export class RegisteredListComponent {
   items: any[] = [];
+  editingIndex: number | null = null;
+  editForm: FormGroup;
 
-  constructor(private itemService: ItemService) {}
-
-  ngOnInit(): void {
-    this.itemService.getItems().subscribe(data => {
-      this.items = data;
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.editForm = this.fb.group({
+      username: [''],
+      password: [''],
+      email: [''],
+      phone: [''],
+      age: [''],
+      musicGenre: ['']
     });
   }
 
-  addItem(item: any) {
-    this.itemService.addItem(item);
+  startEdit(index: number): void {
+    this.editingIndex = index;
+    this.editForm.patchValue(this.items[index]);
+  }
+
+  saveEdit(): void {
+    if (this.editForm.valid && this.editingIndex !== null) {
+      this.items[this.editingIndex] = this.editForm.value;
+      this.editingIndex = null;
+    }
+  }
+
+  cancelEdit(): void {
+    this.editingIndex = null;
+  }
+
+  deleteItem(index: number): void {
+    this.items.splice(index, 1);
+  }
+
+  goToInput(): void {
+    this.router.navigate(['/input']);
   }
 }
